45f22a0a618383c84ebcd7c3a18a7f6cbf889912
Levi Lu <llu@xcalar.com>
2015-04-21 17:23:42 -0700

    add browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,27 @@
 +function(undefined) {
   "use strict";
 
-  var gulp = require("gulp")
-  ,   jade = require("gulp-jade");
+  var gulp       = require("gulp")
+  ,   jade       = require("gulp-jade")
+  ,   browserify = require("gulp-browserify");
 
   gulp.task("jade", function() {
-    gulp.src("./build/jade/*.jade")
+    return gulp.src("./build/jade/*.jade")
       .pipe(jade({
         pretty: true
       }))
       .pipe(gulp.dest("./app/views/"));
   });
 
-  gulp.task("default", ["jade"], function() {
+  gulp.task("browserify", function() {
+    return gulp.src("./app/javascripts/app.js")
+      .pipe(browserify({
+        transform: ["reactify", "envify"]
+      }))
+      .pipe(gulp.dest("./app/javascripts/bundle.js"));
+  });
+
+  gulp.task("default", ["jade", "browserify"], function() {
 
   });
 }();
