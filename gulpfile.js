50bdf742062fc2295a4773ecf8cd64ab26ece3f5
Levi Lu <llu@xcalar.com>
2015-04-21 17:32:39 -0700

    remove gulp-browserify

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,7 @@
   "use strict";
 
   var gulp       = require("gulp")
-  ,   jade       = require("gulp-jade")
-  ,   browserify = require("gulp-browserify");
+  ,   jade       = require("gulp-jade");
 
   gulp.task("jade", function() {
     return gulp.src("./build/jade/*.jade")
@@ -13,13 +12,13 @@
       .pipe(gulp.dest("./app/views/"));
   });
 
-  gulp.task("browserify", function() {
-    return gulp.src("./app/javascripts/app.js")
-      .pipe(browserify({
-        transform: ["reactify", "envify"]
-      }))
-      .pipe(gulp.dest("./app/javascripts/bundle.js"));
-  });
+  // gulp.task("browserify", function() {
+  //   return gulp.src("./app/javascripts/app.js")
+  //     .pipe(browserify({
+  //       transform: ["reactify", "envify"]
+  //     }))
+  //     .pipe(gulp.dest("./app/javascripts/bundle.js"));
+  // });
 
   gulp.task("default", ["jade", "browserify"], function() {
 
